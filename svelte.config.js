64466c91caba9185cca0e24af9fdabf7f8f59385
Nahla E. <12385664+nahlaerrakik@users.noreply.github.com>
2025-04-22 21:15:29 +0200

    Initial commit with basic portfolio structure

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html' // Necessary for single-page apps
+    })
+  }
+};
