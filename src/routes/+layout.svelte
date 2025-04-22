eb07145882a6c322c5dd0ac48f84abe9e8653847
Nahla E. <12385664+nahlaerrakik@users.noreply.github.com>
2025-04-22 22:47:24 +0200

    Added tailwind setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import '$lib/styles/tailwind.postcss';
+</script>
+
+<!-- hared layout structure -->
+<div class="min-h-screen bg-gray-50">
+  <slot /> <!-- This renders +page.svelte content -->
+</div>
